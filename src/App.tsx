502ed9b7b24be11b6c1e50d503b159ac5ffd7582
Durga Chikkala <69903252+Durga-chikkala@users.noreply.github.com>
2025-06-03 19:27:24 +0000

    updated the changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { Header } from './components/layout/Header';
 import { Footer } from './components/layout/Footer';
 import { HeroSection } from './components/sections/HeroSection';
